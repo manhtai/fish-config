1d4fb1e0e6d1c06cba02815e483ea9bc3dd4c13c
manhtai <vo@manhtai.com>
2015-03-18 15:46:52 +0700

    init

---
+++ functions/export.fish
@@ -0,0 +1,25 @@
+# Language Default
+set -x LC_ALL en_US.UTF-8
+set -x LC_CTYPE en_US.UTF-8
+
+# python
+set -x PATH "/usr/local/share/python" $PATH
+
+# respect local bins
+set -x PATH "./bin" $PATH
+
+# editor
+set -x EDITOR "vim"
+
+# Colorful man pages
+# from http://pastie.org/pastes/206041/text
+setenv -x LESS_TERMCAP_mb (set_color -o red)
+setenv -x LESS_TERMCAP_md (set_color -o red)
+setenv -x LESS_TERMCAP_me (set_color normal)
+setenv -x LESS_TERMCAP_se (set_color normal)
+setenv -x LESS_TERMCAP_so (set_color -b blue -o yellow)
+setenv -x LESS_TERMCAP_ue (set_color normal)
+setenv -x LESS_TERMCAP_us (set_color -o green)
+
+# grep colors
+setenv -x GREP_OPTIONS "--color=auto"
